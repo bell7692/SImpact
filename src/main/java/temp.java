e60848bd7be940e4d25dd199eff2781747c87480
Bo Lee <bolee@Bos-MacBook-Pro.local>
2018-10-27 14:38:24 -0400

    adding dependencies on pom file

--- src/main/java/temp.java
+++ src/main/java/temp.java
@@ -12,7 +12,7 @@ public class temp {
 
     public static void main(String[] args) {
 
-        String baseUrl = "https://hackicims.com:8080/api/v1/companies/72";
+        String baseUrl = "https://hackicims.com:8080/api/companies/72";
         String auth = "Bearer f2db6c4321cea9e3d1fa4aa3d68c0b4adf6c317cdaa852500f63c50657c78e30fd7bac056a9dff6cc7035377f7015220fbfd6c368f6eb4fa6833c677c32a3e08\n";
 
         //GET request to get company information
@@ -21,7 +21,11 @@ public class temp {
         //POST request to add a new job called HackHers
         String payload = "{\"title\":\"Artist\"}";
         HttpResponse jobResponse = post(baseUrl+"/jobs", auth, payload);
+        String payload2 = "{\"email\":\"info@google.com\"}";
+        HttpResponse personResponse = post(baseUrl+"/people", auth, payload2);
     }
+
+
 
     public static HttpResponse get(String url, String auth) {
 
